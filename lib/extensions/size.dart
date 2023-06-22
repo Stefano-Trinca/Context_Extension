e8fd57daeb7a84013961d5cd0abbdc1778e154ca
Stefano-Trinca <ste.developer98@gmail.com>
2023-06-22 11:13:40 +0200

    Add CORS Manual + Update Context Extension on Size

--- lib/extensions/size.dart
+++ lib/extensions/size.dart
@@ -2,15 +2,15 @@ import 'package:flutter/widgets.dart';
 
 extension BuildContextEntensionSize<T> on BuildContext {
 
-  bool get isMobile => MediaQuery.of(this).size.width <= 500.0;
+  bool get isMobile => MediaQuery.sizeOf(this).width <= 500.0;
 
-  bool get isTablet => MediaQuery.of(this).size.width < 1024.0 && MediaQuery.of(this).size.width >= 650.0;
+  bool get isTablet => MediaQuery.sizeOf(this).width < 1024.0 && MediaQuery.sizeOf(this).width >= 650.0;
 
-  bool get isSmallTablet => MediaQuery.of(this).size.width < 650.0 && MediaQuery.of(this).size.width > 500.0;
+  bool get isSmallTablet => MediaQuery.sizeOf(this).width < 650.0 && MediaQuery.sizeOf(this).width > 500.0;
 
-  bool get isDesktop => MediaQuery.of(this).size.width >= 1024.0;
+  bool get isDesktop => MediaQuery.sizeOf(this).width >= 1024.0;
 
-  bool get isSmall => MediaQuery.of(this).size.width < 850.0 && MediaQuery.of(this).size.width >= 560.0;
+  bool get isSmall => MediaQuery.sizeOf(this).width < 850.0 && MediaQuery.sizeOf(this).width >= 560.0;
 
   ///screen width
   double get width => MediaQuery.sizeOf(this).width;
